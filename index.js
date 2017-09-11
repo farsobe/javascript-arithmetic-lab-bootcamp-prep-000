4dd8a251af333376f7afd68b442cf3ce22140225
Farrell Sobejana <fsobejana+flatiron@gmail.com>
2017-09-11 17:52:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,35 @@
 
 function add(a,b) {
+  // a function to add two numbers
   return a+b
+}
+
+function subtract(a,b) {
+  // a function to subtract two numbers
+  return a-b
+}
+
+function multiply(a,b) {
+  // a function to multiply two numbers
+  return a*b
+}
+
+function divides(a,b) {
+  // a function to divides two numbers
+  return a/b
+}
+
+function inc(n) {
+  // a function that increments and returns n
+  return n++
+}
+
+function dec(n) {
+  // a function that decrements and returns n
+  return n--
+}
+
+function makeInt(n) {
+  // a function that decrements and returns n
+  return parseInt(n)
 }
